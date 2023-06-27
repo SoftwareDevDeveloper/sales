faca78f5a5704c553c09bd18f7f3bf52b42c151a
Rufus Ajiboye <ruufman@gmail.com>
2023-06-27 16:21:44 +0100

    Sales leverage modules to provision a node

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "provision-ec2" {
+    source = "git::https://github.com/SoftwareDevDeveloper/modules.git"
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  provider "aws" {
+  region = "eu-west-1"
+}
+}
