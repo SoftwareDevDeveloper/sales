61c36d1fe435a5cd5c5f5cd2941296adc44688a6
Rufus Ajiboye <ruufman@gmail.com>
2023-06-28 11:51:35 +0100

    key variable Implemented and updated

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 module "provision_ec1" {
   source = "git::https://github.com/SoftwareDevDeveloper/modules.git"
   machine_name = "Sales first machine"
+  key_name = "test100"
+  instance_type = "t2.micro"
 }
 
 module "provision_ec2" {
   source = "git::https://github.com/SoftwareDevDeveloper/modules.git"
   machine_name = "Sales second machine"
+  key_name = "test100"
+  instance_type = "t2.micro"
 }
 
 terraform {
@@ -17,6 +21,6 @@ terraform {
   }
 }
 
-provider "aws" {
+provider "aws" {   
   region = "eu-west-1"
 }
