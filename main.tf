c383055339bcc8a6674b1c47e6654a92b7b0fe2b
Rufus Ajiboye <ruufman@gmail.com>
2023-06-28 11:06:21 +0100

    Updated sales to override machine name

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 module "provision-ec1" {
   source = "git::https://github.com/SoftwareDevDeveloper/modules.git"
+  machine_name = "first_machine"
 }
 
 module "provision-ec2" {
   source = "git::https://github.com/SoftwareDevDeveloper/modules.git"
+  machine_name = "second_machine"
 }
 
 terraform {
